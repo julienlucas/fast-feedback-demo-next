41135714c4f1184a663f5279b578d1b180bcd87f
Julien Lucas <contact@julienlucas.com>
2021-01-07 11:05:46 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,14 @@
-import { useEffect } from 'react';
 import { ThemeProvider, CSSReset } from '@chakra-ui/core';
 import { Global, css } from '@emotion/core';
 import { MDXProvider } from '@mdx-js/react';
 import { DefaultSeo } from 'next-seo';
 import Head from 'next/head';
-import Router from 'next/router';
-import * as Fathom from 'fathom-client';
 
 import MDXComponents from '@/components/MDXComponents';
 import { AuthProvider } from '@/lib/auth';
 import customTheme from '@/styles/theme';
 
 import SEO from '../next-seo.config';
-
-Router.events.on('routeChangeComplete', () => {
-  Fathom.trackPageview();
-});
 
 const GlobalStyle = ({ children }) => {
   return (
@@ -43,14 +36,6 @@ const GlobalStyle = ({ children }) => {
 };
 
 const App = ({ Component, pageProps }) => {
-  useEffect(() => {
-    if (process.env.NODE_ENV === 'production') {
-      Fathom.load(process.env.NEXT_PUBLIC_FATHOM_SITE_ID, {
-        includedDomains: ['fastfeedback.io']
-      });
-    }
-  }, []);
-
   return (
     <ThemeProvider theme={customTheme}>
       <AuthProvider>
